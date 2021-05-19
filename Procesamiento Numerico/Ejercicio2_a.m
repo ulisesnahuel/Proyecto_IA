19b9294e34ba5bd566dad1764b75e88c4a3ca25d
jnmendozarg <84452963+jnmendozarg@users.noreply.github.com>
2021-05-19 12:30:00 -0300

    Update Ejercicio2_a.m

--- Procesamiento Numerico/Ejercicio2_a.m
+++ Procesamiento Numerico/Ejercicio2_a.m
@@ -1,11 +1,11 @@
-x=linspace(0,2*pi,100) %se crea un serie de 100 n鷐eros
-y1=sin(x) 
-y2=cos(x)
-y3=tan(x)
-y4=sec(x)
-plot(x,y1) %se grafica la funci髇 seno
+x=linspace(0,2*pi,100) %se crea un serie de 100 n煤meros
+y1=sin(x); 
+y2=cos(x);
+y3=tan(x);
+y4=sec(x);
+plot(x,y1) %se grafica la funci贸n seno
 hold on
-plot(x,y2) %se grafica la funci髇 coseno
-plot(x,y3,'g','LineWidth',2) %se grafica la funci髇 tangente 
-plot(x,y4,'bd','LineWidth',2) %se grafica la funci髇 secante
+plot(x,y2) %se grafica la funci贸n coseno
+plot(x,y3,'g','LineWidth',2) %se grafica la funci贸n tangente 
+plot(x,y4,'bd','LineWidth',2) %se grafica la funci贸n secante
 title('Funciones')
