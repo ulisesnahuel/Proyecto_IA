ebfb33e7105582c679e1eed49496c7750ae3dc72
jnmendozarg <84452963+jnmendozarg@users.noreply.github.com>
2021-05-19 12:48:42 -0300

    Update Ejercicio6_cd.m

--- Procesamiento Numerico/Ejercicio6_cd.m
+++ Procesamiento Numerico/Ejercicio6_cd.m
@@ -1,7 +1,9 @@
-t = linspace(0,2.094450)
-y = 10 * sin (2*t)
-ruido = awgn(y,0.1)
+t = linspace(0,2.094450);
+y = 10 * sin (2*t);
+ruido = awgn(y,0.1);
 plot (t,y,'r',t,ruido,'b','linewidth',3)
 grid on
-u = mean (t) % media
-o = std(t) %Desviacion estandar
+u = mean (t); % Valor de la Media
+o = std(t); %Valor de la Desviación estándar
+u2 = mean(ruido);
+o2 = std(ruido);
