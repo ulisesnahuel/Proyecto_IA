341399f0db2b2df8a57289066652cee0d91bacbb
jnmendozarg <84452963+jnmendozarg@users.noreply.github.com>
2021-05-19 12:44:15 -0300

    Update Ejercicio5_b.m

--- Procesamiento Numerico/Ejercicio5_b.m
+++ Procesamiento Numerico/Ejercicio5_b.m
@@ -1,9 +1,9 @@
-x=linspace(-10,10,100)
-y=cos(3*x).*exp(-x.^2/2)
+x=linspace(-10,10,100);
+y=cos(3*x).*exp(-x.^2/2);
 plot(x,y)
 hold on
-y1=mapminmax(y,-1,0)
+y1=mapminmax(y,-1,0);
 plot(x,y1)
 xlabel('X')
 ylabel('Y')
-title('Reescalado de función wavelet Morlet')
+title('Reescalado de funciÃ³n wavelet Morlet')
