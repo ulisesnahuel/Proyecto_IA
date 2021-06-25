15848735fdfac4dcdc68c1a5dc3f15f813c72593
UlisesSSJ27 <84428849+UlisesSSJ27@users.noreply.github.com>
2021-06-24 22:47:22 -0300

    Update Ejercicio6_cd.m

--- Procesamiento Numerico/Ejercicio6_cd.m
+++ Procesamiento Numerico/Ejercicio6_cd.m
@@ -1,9 +1,9 @@
-t = linspace(0,2.094450);
-y = 10 * sin (2*t);
-ruido = awgn(y,0.1);
+t = linspace(0,2.094450)
+y = 10 * sin (2*t)
+u = mean(y);
+o = std(y);
+ruido = awgn(y,10,'measured')
 plot (t,y,'r',t,ruido,'b','linewidth',3)
 grid on
-u = mean (t); % Valor de la Media
-o = std(t); %Valor de la Desviación estándar
-u2 = mean(ruido);
-o2 = std(ruido);
+u = mean (t) % media
+o = std(t) %Desviacion estandar
